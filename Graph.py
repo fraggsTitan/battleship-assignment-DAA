c7b617d6e47c3be0948d561f2515dd55c19069f3
fraggsTitan <vishnu.sara.kuma@gmail.com>
2025-12-19 15:08:40 +0530

    Intiialised class with hitter method for graph.py

--- Graph.py
+++ Graph.py
@@ -1,6 +1,5 @@
 from States import CellState, AIModeS
 
-
 class Graph:
 
     class Cell:
@@ -11,3 +10,17 @@ class Graph:
 
         def set_state(self, state):
             self.state = state
+    def __init__(self):
+        self.grid=[[]]
+        grid=10*[]
+        for i in range(10):
+            grid[i]=10*[]
+            for j in range (10):
+                grid[i][j]=Graph.Cell(i,j)
+
+    def setHit(self,i,j,state):
+        if(i>=10 or j>=10):
+            IndexError("Illegal index")
+        self.grid[i][j].set_state(state)
+    
+            
