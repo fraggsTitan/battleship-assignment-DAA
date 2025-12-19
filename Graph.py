985fe517f08e163d3c697d51bd8c5d29f5b89bbf
fraggsTitan <vishnu.sara.kuma@gmail.com>
2025-12-19 14:58:15 +0530

    Local changes to Graph.py

--- Graph.py
+++ Graph.py
@@ -1,13 +1,10 @@
 from States import CellState,AIModeS
 class Graph:
     class Cell:
-        i=0
-        j=0 
-        state=CellState.UNKNOWN
         def __init__(self,i,j):
             self.i=i
             self.j=j
-        def setState(state):
+        def setState(self,state):
             self.state=state
 
         
